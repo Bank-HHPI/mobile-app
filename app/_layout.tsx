cf91a90f3440c39f5d0bdc766b0a1d36734d7075
Matsuel <matsuel0308@gmail.com>
2025-03-12 14:19:40 +0100

    Page profile -> Creation de la page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,11 @@ export default function Layout() {
       <Stack.Screen options={{ headerShown: false, gestureEnabled: false }} name='welcome' />
       <Stack.Screen options={{ headerShown: false, gestureEnabled: false }} name='home' />
       <Stack.Screen options={{ headerShown: false, gestureEnabled: false, presentation: "transparentModal" }} name='accountinfos' />
+      <Stack.Screen name='profile' options={{
+        headerShown: false, gestureEnabled: true, presentation: "modal", contentStyle: {
+          height: "100%", maxHeight: "100%", justifyContent: "flex-end", backgroundColor: "transparent"
+        },
+      }} />
     </Stack>
   );
 }
