46e23c62e7dc19dd679e10fb9588306343e9478b
Matsuel <matsuel0308@gmail.com>
2025-03-12 11:46:30 +0100

    Creation composants Infos

--- app/accountinfos.tsx
+++ app/accountinfos.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Infos from '~/components/AccountInfos/Infos'
 import BackButton from '~/components/BackButton'
 
 interface AccountInfosProps {
@@ -11,15 +12,31 @@ interface AccountInfosProps {
     }
 }
 
-const accountInfos = ({
-    accountInfos
-}: AccountInfosProps) => {
+const accountInfos = () => {
+
+    const accountInfos = {
+        beneficiary: "John Doe",
+        iban: "FR76 3000 3000 3000 3000 3000 3000 300",
+        bic: "BNPAFRPPXXX",
+        address: "5 rue de la paix, 75000 Paris"
+    }
+
     return (
         <View style={styles.container}>
             <BackButton />
             <Text style={styles.title}>
                 Informations du compte
             </Text>
+
+            <View style={styles.accountInfos}>
+                <Text style={styles.accountInfosTitle}>
+                    Virements nationaux
+                </Text>
+                <Infos
+                    title="Bénéficiaire"
+                    value={accountInfos.beneficiary}
+                />
+            </View>
         </View>
     )
 }
@@ -42,5 +59,23 @@ export const styles = StyleSheet.create({
         fontWeight: "700",
         color: "#000",
         marginTop: 10,
+    },
+    accountInfos: {
+        width: "100%",
+        height: "auto",
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        backgroundColor: "#fff",
+        marginTop: 20,
+        borderRadius: 16,
+        paddingVertical: 20,
+        paddingHorizontal: 20,
+        gap: 25,
+    },
+    accountInfosTitle: {
+        fontSize: 20,
+        fontWeight: "400",
+        color: "#000",
     }
 })
